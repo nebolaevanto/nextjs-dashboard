b4fe7af09db0ace2fe5cbb2c5767551a425d9fe3
nebolaevanto <8833037+nebolaevanto@users.noreply.github.com>
2025-12-08 14:59:32 +0530

    Add dashboard layout and pages with navigation links

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Roboto, Lusitana } from "next/font/google";
+
+// export const inter = Inter({ subsets: ["latin"] });
+export const roboto = Roboto({ subsets: ["latin"] });
+
+export const lusitana = Lusitana({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
